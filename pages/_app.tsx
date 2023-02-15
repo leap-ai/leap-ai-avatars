b2cbc845e50a33b266e08255605c67c6a6e71a6f
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2023-02-15 00:05:04 -0800

    Deploy first version!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
---
+++ pages/api/generate.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Leap } from "@leap-ai/sdk";
+
+const MODEL_ID = "e62a3f3f-cce4-4cac-826b-d22e93f687d4";
+const IMAGE_WIDTH = 600;
+const IMAGE_HEIGHT = 400;
+
+const generate = async (req: NextApiRequest, res: NextApiResponse) => {
+  const prompt = req.body.prompt as string;
+  const apiKey = process.env.LEAP_API_KEY as string;
+
+  if (!prompt || prompt.length === 0 || !apiKey) {
+    res.status(400).json({ error: "Invalid request. Check key and prompt." });
+    return;
+  }
+
+  const leap = new Leap(apiKey);
+
+  const { data, error } = await leap.generate.generateImage({
+    modelId: MODEL_ID,
+    prompt,
+    width: IMAGE_WIDTH,
+    height: IMAGE_HEIGHT,
+  });
+
+  if (error) {
+    res.status(500).json(error);
+    return;
+  }
+
+  res.status(200).json(data);
+};
+
+export default generate;
